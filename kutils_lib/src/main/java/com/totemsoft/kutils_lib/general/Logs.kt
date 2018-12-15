9151a0ef5c3fdfeb6f0dc43514ced5e88334433a
Tonia Tkachuk <tonia.tkachuk@gmail.com>
2018-12-15 13:13:41 +0100

    warning log

--- kutils_lib/src/main/java/com/totemsoft/kutils_lib/general/Logs.kt
+++ kutils_lib/src/main/java/com/totemsoft/kutils_lib/general/Logs.kt
@@ -8,4 +8,6 @@ fun Any.logD(message: String) { Log.d(this.javaClass.simpleName, message) }
 
 fun Any.logI(message: String) { Log.i(this.javaClass.simpleName, message) }
 
+fun Any.logW(message: String) { Log.w(this.javaClass.simpleName, message) }
+
 fun Any.logE(message: String) { Log.e(this.javaClass.simpleName, message) }
